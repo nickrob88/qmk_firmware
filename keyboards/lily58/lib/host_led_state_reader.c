3b99e31350e0177c3ffec4b682fcdd39494a48fe
Nick Roberts (nickrobertssky) <nick.roberts@sky.uk>
2021-05-25 09:13:30 +0100

    my keymap

--- keyboards/lily58/lib/host_led_state_reader.c
+++ keyboards/lily58/lib/host_led_state_reader.c
@@ -7,10 +7,10 @@ char host_led_state_str[24];
 
 const char *read_host_led_state(void)
 {
-  snprintf(host_led_state_str, sizeof(host_led_state_str), "NL:%s CL:%s SL:%s",
-           (IS_HOST_LED_ON(USB_LED_NUM_LOCK)) ? "on" : "- ",
-           (IS_HOST_LED_ON(USB_LED_CAPS_LOCK)) ? "on" : "- ",
-           (IS_HOST_LED_ON(USB_LED_SCROLL_LOCK)) ? "on" : "- ");
+  snprintf(host_led_state_str, sizeof(host_led_state_str), "%s %s %s",
+           (IS_HOST_LED_ON(USB_LED_CAPS_LOCK)) ? "CAPS" : " ",
+           (IS_HOST_LED_ON(USB_LED_NUM_LOCK)) ? "NUM" : " ",
+           (IS_HOST_LED_ON(USB_LED_SCROLL_LOCK)) ? "SCROLL" : " ");
 
   return host_led_state_str;
 }
--- keyboards/lily58/lib/mode_icon_reader.c
+++ keyboards/lily58/lib/mode_icon_reader.c
@@ -2,15 +2,26 @@
 #include <stdbool.h>
 #include "lily58.h"
 
-char mode_icon[24];
+char mode_icon_top[10];
+char mode_icon_bottom[10];
+static char logo[][2][3] = {{{0x95, 0x96, 0}, {0xb5, 0xb6, 0}}, {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}}};
 
-const char *read_mode_icon(bool swap) {
-  static char logo[][2][3] = {{{0x95, 0x96, 0}, {0xb5, 0xb6, 0}}, {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}}};
+const char *read_mode_icon_top(bool swap) {
   if (swap == false) {
-    snprintf(mode_icon, sizeof(mode_icon), "%s\n%s", logo[0][0], logo[0][1]);
+    snprintf(mode_icon_top, sizeof(mode_icon_top), "%s ", logo[0][0]);
   } else {
-    snprintf(mode_icon, sizeof(mode_icon), "%s\n%s", logo[1][0], logo[1][1]);
+    snprintf(mode_icon_top, sizeof(mode_icon_top), "%s ", logo[1][0]);
   }
 
-  return mode_icon;
+  return mode_icon_top;
+}
+
+const char *read_mode_icon_bottom(bool swap) {
+  if (swap == false) {
+    snprintf(mode_icon_bottom, sizeof(mode_icon_bottom), "%s ", logo[0][1]);
+  } else {
+    snprintf(mode_icon_bottom, sizeof(mode_icon_bottom), "%s ", logo[1][1]);
+  }
+
+  return mode_icon_bottom;
 }
